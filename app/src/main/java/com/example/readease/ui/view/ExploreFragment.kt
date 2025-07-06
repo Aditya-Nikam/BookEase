0512af714071b3be1dcb71f48a49151ba31567fa
Aditya Nikam <adityanikam4047@gmail.com>
2025-07-06 21:43:35 +0530

    basic working completed

--- app/src/main/java/com/example/readease/ui/view/ExploreFragment.kt
+++ app/src/main/java/com/example/readease/ui/view/ExploreFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.readease.R
+import com.example.readease.data.db.AppDatabase
 import com.example.readease.data.model.Books
 import com.example.readease.databinding.FragmentExploreBinding
+import com.example.readease.repository.BooksRepository
+import com.example.readease.ui.viewmodel.BooksViewModel
+import com.example.readease.ui.viewmodel.BooksViewModelFactory
 
 class ExploreFragment : Fragment() {
 
     private var _binding: FragmentExploreBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var book: Books
+    private lateinit var viewModel: BooksViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,23 +31,39 @@ class ExploreFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentExploreBinding.bind(view)
+        // Setup ViewModel
+        val dao = AppDatabase.getDatabase(requireContext()).booksDao()
+        val repository = BooksRepository(dao)
+        val factory = BooksViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, factory)[BooksViewModel::class.java]
 
-        val book = arguments?.getParcelable<Books>("book")
-        book?.let {
-            binding.tvTitle.text = it.title
-            binding.tvAuthor.text = it.subject
-            binding.tvOverview.text = it.category
-
-            val secureUrl = it.thumbnail.replace("http://", "https://")
-            Glide.with(requireContext())
-                .load(secureUrl)
-                .placeholder(R.drawable.image)
-                .into(binding.ivCover)
+        val bookFromArgs = arguments?.getParcelable<Books>("book")
+        if (bookFromArgs != null) {
+            bindBook(bookFromArgs)
+        } else {
+            // Load first book from DB if no book passed
+            viewModel.books.observe(viewLifecycleOwner) { books ->
+                if (books.isNotEmpty()) {
+                    bindBook(books[0])
+                }
+            }
+            viewModel.loadBooks()
         }
     }
 
+    private fun bindBook(book: Books) {
+        binding.tvTitle.text = book.title
+        binding.tvAuthor.text = book.author
+        binding.tvSummary.text = book.subject
+        binding.tvOverview.text = book.description
+
+        val secureUrl = book.thumbnail.replace("http://", "https://")
+        Glide.with(requireContext())
+            .load(secureUrl)
+            .placeholder(com.example.readease.R.drawable.image)
+            .error(com.example.readease.R.drawable.image)
+            .into(binding.ivCover)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
